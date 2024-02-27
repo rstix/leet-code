e6bc3fe83bda2442510f84ed71af1192b7e1aba8
Roman Stix <rstix@student.42heilbronn.de>
2024-02-27 12:42:15 +0100

    naive approach done

--- two-sum/two_sum.js
+++ two-sum/two_sum.js
@@ -3,6 +3,34 @@
  * @param {number} target
  * @return {number[]}
  */
+
+// naive approach, 2 loops 
+// time: O(n^2)
+// space: O(1)
 var twoSum = function(nums, target) {
-    
+    const len = nums.length;
+	if(len < 2) return;
+
+	const res = []
+	let i = 0;
+	
+	while(i < len - 1){
+		let j = i+1;
+		while(j < len){
+			if (nums[i] + nums[j] == target){
+				res.push(i);
+				res.push(j);
+				break;
+			}
+			j++;
+		}
+		if(res.length == 2) break;
+		i++;
+	}
+	return res;
 };
+
+const nums = [3,2,4];
+const target = 6;
+
+console.log(twoSum(nums, target));
