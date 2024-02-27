ad4d7dcd3510956e7a10c109c257084c1db87e25
Roman Stix <rstix@student.42heilbronn.de>
2024-02-27 13:17:53 +0100

    another approach

--- two-sum/two_sum.js
+++ two-sum/two_sum.js
@@ -30,7 +30,21 @@ var twoSum = function(nums, target) {
 	return res;
 };
 
-const nums = [3,2,4];
+// always checking the difference
+var twoSum = function(nums, target) {
+	const len = nums.length;
+	let i = 0;
+	let indexFind;
+	
+	while(i < len){
+		indexFind = nums.indexOf(target - nums[i])
+		if(indexFind != -1 && indexFind != i) break;
+		i++; 
+	}
+	return [i, indexFind].sort();
+};
+
+const nums = [3,2,3];
 const target = 6;
 
-console.log(twoSum(nums, target));
+console.log(twoSum([3,2,4], 6));
